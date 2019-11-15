3af1989b81e3f3ac879ab13834bb8ee130aa9fee
Martin <mpiccato@me.com>
2019-11-15 14:19:25 -0300

    Parece que va a arreglar el tema de ordenar por fecha s#ource ~/.bas

--- graficarpeso.py
+++ graficarpeso.py
@@ -1,24 +1,35 @@
 import sqlite3
 import time
 import datetime
-
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+
+strNombre = ""
 
 
 MiConexion = sqlite3.connect('ALQUISTA.DB')
 miCursor = MiConexion.cursor()
 
+
+
+
 def graficar_datos():
-    miCursor.execute("SELECT * FROM PESOALQUISTA ORDER BY FECHAPESO ASC")
+     #veo si la consulta está vacía, sino limpiarla primero
+  
+
+    miCursor.execute("SELECT * FROM PESOALQUISTA WHERE NROALQUISTA = 120 ORDER BY FECHAPESO")
     #miCursor.execute("SELECT * FROM PESOALQUISTA")
+  
     fechas = []
     pesos = []
     for filas in miCursor.fetchall():
         print("Hola {}, tus fechas y pesos son: {} -> {}".format(filas[0],filas[3],filas[4]))
         fechas.append(filas[3])
         pesos.append(filas[4])
+
+    
+    #plt.title("Evolución del peso de {} {}".format(strNombre[0],strNombre[1]))
     plt.xlabel("Fechas del peso")
     plt.ylabel("Peso observado")
     plt.plot(fechas,pesos)
